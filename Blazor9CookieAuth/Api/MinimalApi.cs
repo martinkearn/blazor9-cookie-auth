30e7e8aac5bd8d6bb60fadaac76067fd2fd940ce
Martin Kearn <martinkearn@live.co.uk>
2025-07-30 21:01:34 +0100

    Minimal api

--- Blazor9CookieAuth/Api/MinimalApi.cs
+++ Blazor9CookieAuth/Api/MinimalApi.cs
@@ -10,5 +10,7 @@ public static class MinimalApi
         
         // Access as curl -i http://localhost:5269/api/minimal,
         // By default, this will return HTTP/1.1 401 Unauthorized
+        // To make an authenticated request, first get a cookie: curl -i -X POST http://localhost:5269/api/auth/login -H "Content-Type: application/json" -d '"foo"'
+        // Then make the request with the --cookie parameter and the full "Set-Cookie" output of api/auth/login: curl -i http://localhost:5269/api/minimal --cookie "your Set-Cookie value here" 
     }
 }
