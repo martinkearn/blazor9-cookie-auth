b81fe510820ff16c208c0a6f0564595805b38a8b
Martin Kearn <martinkearn@live.co.uk>
2025-07-30 21:20:27 +0100

    Client calling api

--- Blazor9CookieAuth/Api/MinimalApi.cs
+++ Blazor9CookieAuth/Api/MinimalApi.cs
@@ -6,11 +6,11 @@ public static class MinimalApi
 {
     public static void MapMinimalApiEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/api/minimal", [Authorize] () => Results.Ok("You are authenticated on MinimalApi")); 
+        app.MapGet("/api/minimalapi", [Authorize] () => Results.Ok("You are authenticated on MinimalApi")); 
         
-        // Access with: curl -i http://localhost:5269/api/minimal,
+        // Access with: curl -i http://localhost:5269/api/minimalapi,
         // By default, this will return HTTP/1.1 401 Unauthorized
         // To make an authenticated request, first get a cookie: curl -i -X POST http://localhost:5269/api/auth/login -H "Content-Type: application/json" -d '"foo"'
-        // Then make the request with the --cookie parameter and the full "Set-Cookie" output of api/auth/login: curl -i http://localhost:5269/api/minimal --cookie "your Set-Cookie value here" 
+        // Then make the request with the --cookie parameter and the full "Set-Cookie" output of api/auth/login: curl -i http://localhost:5269/api/minimalapi --cookie "your Set-Cookie value here" 
     }
 }
